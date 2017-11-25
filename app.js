ec6994336cea0d018f51c80ad66063e2618496ba
lanleilin <2476081528@qq.com>
2017-11-25 11:45:58 +0800

    ws
    Change-Id: I07c9dd259ea75ddb130ba312e04662d449c88565

--- app.js
+++ app.js
@@ -35,61 +35,8 @@ app.use(rest.restify());
 // add controllers:
 app.use(controller());
 
-// // 数据库
-// const Sequelize = require('sequelize');
-// const config = require('./config');
-// // 创建一个sequelize对象实例
-// var sequelize = new Sequelize(config.database, config.username, config.password, {
-//     host: config.host,
-//     dialect: 'mysql',
-//     pool: {
-//         max: 5,
-//         min: 0,
-//         idle: 30000
-//     }
-// });
-// // 定义模型Pet，告诉Sequelize如何映射数据库表
-// var Pet = sequelize.define('pet', {
-//     id: {
-//         type: Sequelize.STRING(50),
-//         primaryKey: true
-//     },
-//     name: Sequelize.STRING(100),
-//     gender: Sequelize.BOOLEAN,
-//     birth: Sequelize.STRING(10),
-//     createdAt: Sequelize.BIGINT,
-//     updatedAt: Sequelize.BIGINT,
-//     version: Sequelize.BIGINT
-// }, {
-//     timestamps: false
-// });
-// 存入
-// var now = Date.now();
-// (async() => {
-//     var dog = await Pet.create({
-//         id: 'd-' + now,
-//         name: 'little_dog',
-//         gender: false,
-//         birth: '2017-08-08',
-//         createdAt: now,
-//         updatedAt: now,
-//         version: 0
-//     });
-//     console.log('created: ' + JSON.stringify(dog));
-// })();
-// // 查询
-// (async() => {
-//     var pets = await Pet.findAll({
-//         where: {
-//             name: 'little_dog'
-//         }
-//     });
-//     console.log(`find ${pets.length} pets:`);
-//     for (let p of pets) {
-//         console.log('++++++++++++++++++++++++++++++++++++++++++++')
-//         console.log(JSON.stringify(p));
-//     }
-// })();
+
+
 
 app.listen(3001);
 console.log('app started at port 3001...');

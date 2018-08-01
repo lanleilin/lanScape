6fbe9229acb214fe1beda992f2f59cceeb3704bd
lanleilin <2476081528@qq.com>
2018-08-02 01:34:13 +0800

    work

---
+++ static/samples/productList/_id.vue
@@ -0,0 +1,265 @@
+<template>
+  <div class="productDetail">
+    <div class="header">
+      <div class="leftSide">
+        <span class="title mr10">浏览产品</span>
+        <i class="el-icon-caret-right mr10"></i>
+        <span class="mr10">TSD-104</span>
+      </div>
+      <div class="rigthSide">
+        <div class="counter" style="cursor: pointer;">10</div>
+        <img src="../../assets/cart.jpg" alt="" class="cart" style="cursor: pointer;">
+      </div>
+    </div>
+    <div class="subTitle">Acidibacillus sulfuroxidans (TSD-104)</div>
+    <div class="content">
+      <el-row>
+        <el-col :sm="24" :md="20">
+          <div class="leftContent">
+            <ul class="tabs">
+              <li @click="changeTabs('baseInfo')" :class="{ active: tabsType === 'baseInfo' }">基本信息</li>
+              <li @click="changeTabs('cultivating')" :class="{ active: tabsType === 'cultivating' }">培养方式</li>
+              <li @click="changeTabs('history')" :class="{ active: tabsType === 'history' }">历史</li>
+            </ul>
+            <div class="mainContent">
+              <div v-if="tabsType==='baseInfo'"> 
+                <el-table :data="tableData" style="width: 100%">
+                  <el-table-column prop="name" width="130"></el-table-column>
+                  <el-table-column prop="inf" ></el-table-column>
+                </el-table>
+              </div>
+              <div v-else-if="tabsType==='cultivating'">
+                cultivating
+              </div>
+              <div v-else-if="tabsType==='history'">
+                history
+              </div>
+            </div>
+          </div>
+        </el-col>
+        <el-col :sm="24" :md="4">
+          <div class="rightContent">
+            <div class="part">
+              <div>产品名</div>
+              <div>Acidibacillus sulfuro-xidans(TSD-104)</div>
+            </div>
+            <div class="part">
+              <div>产品单价</div>
+              <div>¥600</div>
+            </div>
+            <div class="part">
+              <div>数量</div>
+              <div class="numInput">
+                <el-input v-model="counter" placeholder="请输入数量"></el-input>
+                <i class="el-icon-caret-top numadd" @click="addnum()"></i>
+                <i class="el-icon-caret-bottom numminus" @click="minusnum()"></i>
+              </div>
+            </div>
+            <div class="addCart">加入购物车</div>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'header',
+  data () {
+    return {
+      tableData: [{
+        name: '菌种分离度',
+        inf: 'Y002'
+      }, {
+        name: '生物安全级别',
+        inf: '上海市普陀区金沙江路 1517 弄上海市普陀区金沙江路 1517 弄上海市普陀区金沙江路 1517 弄上海市普陀区金沙江路 1517 弄上海市普陀区金沙江路 1517 弄上海市普陀区金沙江路 1517 弄上海市普陀区金沙江路 1517 弄上海市普陀区金沙江路 1517 弄上海市普陀区金沙江路 1517 弄上海市普陀区金沙江路 1517 弄上海市普陀区金沙江路 1517 弄上海市普陀区金沙江路 1517 弄上海市普陀区金沙江路 1517 弄上海市普陀区金沙江路 1517 弄上海市普陀区金沙江路 1517 弄上海市普陀区金沙江路 1517 弄上海市普陀区金沙江路 1517 弄上海市普陀区金沙江路 1517 弄上海市普陀区金沙江路 1517 弄'
+      }, {
+        name: '产品状态',
+        inf: '冷冻'
+      }, {
+        name: '产品状态',
+        inf: '冷冻'
+      }, {
+        name: '产品状态',
+        inf: '冷冻'
+      }, {
+        name: '产品状态',
+        inf: '冷冻'
+      }, {
+        name: '产品状态',
+        inf: '冷冻'
+      }],
+      tabsType: 'baseInfo',
+      counter: 1
+    }
+  },
+  methods: {
+    changeTabs (type) {
+      this.tabsType = type
+    },
+    addnum () {
+      this.counter = ++this.counter
+    },
+    minusnum () {
+      if(this.counter <= 1) return
+      this.counter = --this.counter
+    }
+  }
+}
+</script>
+<style lang='less'>
+  .productDetail .el-table thead {
+    display: none;
+  }
+  .productDetail .el-table td {
+    vertical-align: top;
+    border-bottom: 1px solid #b8b8b8;
+    &:first-child {
+      font-weight: 700;
+    }
+  }
+  .productDetail .el-table::before {
+    height: 0;
+  }
+  .productDetail .el-input__inner {
+    color: #52c0d0;
+    font-weight: 700;
+    width: 120px;
+    height: 32px;
+    border-radius: 16px;
+  }
+</style>
+<style lang="less" scoped>
+// @import url('../../styles/mixins.less');
+  .mr10 {
+    margin-right: 10px;
+  }
+  .productDetail {
+    background: #f4f4f4;
+    position: relative;
+    box-sizing: content-box;
+    max-width: 1300px;
+    margin: 0 auto;
+    .header {
+      color: #52c0d0;
+      height: 80px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      .rigthSide {
+        position: relative;
+        .counter {
+          font-size: 14px;
+          background: #52c0d0;
+          display: inline-block;
+          border-radius: 10px;
+          width: 40px;
+          height: 20px;
+          color: #fff;
+          text-align: center;
+          margin-right: 36px;
+        }
+        .cart {
+          position: absolute;
+          top: -2px;
+          right: 2px;
+          width: 30px;
+        }
+      }
+    }
+    .subTitle {
+      line-height: 50px;
+    }
+    .content {
+      position: relative;
+      overflow: hidden;
+      // display: flex;
+      .leftContent {
+        // float: left;
+        // width: calc(100% - 270px);
+        // flex: 1;
+        margin-right: 20px;
+        .tabs {
+          overflow: hidden;
+          padding-left: 0;
+          width: 100%;
+          li {
+            float: left;
+            text-align: center;
+            width: 110px;
+            background: #383d41;
+            color: #fff;
+            line-height: 30px;
+            margin-left: 2px;
+            font-weight: 700;
+            font-size: 12px;
+            cursor: pointer;
+            &.active {
+              background: #fff;
+              color: #52c0d0;
+            }
+          }
+        }
+        .mainContent {
+          background-color: #fff;
+          padding: 30px 24px;
+        }
+      }
+      .rightContent {
+        // float: right;
+        margin-top: 30px;
+        // width: 250px;
+        border: 1px solid;
+        background: #383d41;
+        padding: 40px 20px;
+        color: #fff;
+        font-weight: 700;
+        .part {
+          font-size: 16px;
+          margin-bottom: 50px;
+          div:first-child {
+            color: #52c0d0;
+            font-size: 12px;
+            margin-bottom: 4px;
+            font-weight: 700;
+          }
+          .numInput {
+            position: relative;
+            max-width: 120px;
+            .numadd, .numminus {
+              position: absolute;
+              right: 10px;
+              font-size: 18px;
+              color: #52c0d0;
+              cursor: pointer;
+            }
+            .numadd {
+              top: 0px;
+            }
+            .numminus {
+              bottom: 6px;
+            }
+          }
+        }
+        .addCart{
+          background: #5dc2d1;
+          line-height: 36px;
+          border-radius: 18px;
+          font-size: 14px;
+          text-align: center;
+          cursor: pointer;
+          // max-width: 160px;
+          // margin: 0 auto;
+          // .btn_d()
+        }
+      }
+    }
+  }
+  @media (max-width: 992px) {
+    .productDetail .content .leftContent {
+      margin-right: 0;
+    }
+  }
+</style>
+
+
---
+++ static/samples/productList/index.vue
@@ -0,0 +1,306 @@
+<template>
+  <section class="container">
+    <div class="content-container">
+      <section class="menu-container">
+        <div class="menu-type">
+          <div class="menu-item card-view">
+            <el-select v-model="value" placeholder="卡片浏览">
+              <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+              </el-option>
+            </el-select>
+          </div>
+          <div class="menu-item">
+            <span>搜索:</span>
+            <input type="text" placeholder="" class="moon-search-input">
+          </div>
+        </div>
+        
+        <div class="menu-type sort-view">
+          <div class="menu-item">
+          
+            <el-select v-model="sort_value" placeholder="排序">
+              <el-option
+                v-for="item in sortOption"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+              </el-option>
+            </el-select>
+          </div>
+          <div class="menu-item">
+            <span class="cart-num">10</span>
+            <img src="../../assets/cart.jpg" alt="" class="cart" style="cursor: pointer;">
+          </div>
+        </div>
+      </section>
+      <section v-if="value==='2'">
+        <Table></Table>
+      </section>
+      <section class="list-container" v-else>
+        <div class="list-item" v-for="(item,index) in productLists" :key="index">
+          <p class="item-avatar">
+            <span>{{ item.des }}</span>
+          </p>
+          <p class="item-name">{{ item.seq }}</p>
+          <p class="item-parice">¥ {{item.price}}</p>
+        </div>
+        
+      </section>
+      <section class="load-more">
+        <p>加载更多</p>
+      </section>
+    </div>
+  </section>
+</template>
+
+<script>
+import Table from '~/components/product/table'
+
+export default {
+  name: 'product-list',
+  components: {
+    Table,
+  },
+  data () {
+    return{
+      value: '',
+      sort_value: '',
+      options: [{
+        value: '1',
+        label: '卡片浏览'
+      }, {
+        value: '2',
+        label: '列表浏览'
+      }],
+      sortOption: [{
+        value: '3',
+        label: '字母排序'
+      }, {
+        value: '4',
+        label: '价格排序'
+      }],
+      productLists: [{
+        des: 'long and conplex name',
+        seq: 'JDV-V-675',
+        price: '400',
+      }, {
+        des: 'long and conplex name',
+        seq: 'JDV-V-675',
+        price: '400',
+      }, {
+        des: 'long and conplex name',
+        seq: 'JDV-V-675',
+        price: '400',
+      }, {
+        des: 'long and conplex name',
+        seq: 'JDV-V-675',
+        price: '400',
+      }, {
+        des: 'long and conplex name',
+        seq: 'JDV-V-675',
+        price: '400',
+      }, ]
+    }
+  }
+}
+</script>
+
+
+<style lang="less" scoped>
+.menu-container{
+  padding: 0 30px;
+}
+.container{
+  background: #f6f6f6;
+}
+.content-container{
+  max-width: 1300px;
+}
+.menu-type{
+  display: flex;
+  justify-content: space-between;
+  background: #f6f6f6;
+  max-width: 1300px;
+  padding: 6px 0;
+}
+.list-container{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  .list-item{
+    width: 200px;
+    height: 200px;
+    margin: 5px;
+    background: #ffffff;
+    border-radius: 5px;
+    box-shadow: #d1d1d1 0px 1px 2px 0px;
+    display: flex;
+    flex-direction: column;
+    .item-avatar{
+      margin: 20px 0;
+      padding: 4px;
+      width: 110px;
+      height: 110px;
+      border-radius: 50%;
+      background: #c3c3c3;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      span{
+        color: #5b5e61;
+        display: block;
+        text-align: center;
+      }
+    }
+    p{
+      align-self:center;
+      margin: 5px 0;
+    }
+    .item-name{
+      color: #60c3d2;
+    }
+    
+  }
+}
+.load-more{
+  width: 100%;
+  padding: 10px 0;
+  p{
+    margin: 0 auto;
+    width: 240px;
+    height: 30px;
+    border-radius: 30px;
+    border: 2px solid #60c3d2;
+    color: #60c3d2;
+    text-align: center;
+    line-height: 26px;
+  }
+}
+// 菜单栏
+
+.moon-search-input{
+    -webkit-appearance: none;
+    background-color: #fff;
+    background-image: none;
+    border-radius: 4px;
+    border: 1px solid #60c3d2;
+    box-sizing: border-box;
+    color: #606266;
+    display: inline-block;
+    font-size: inherit;
+    height: 24px;
+    line-height: 24px;
+    outline: none;
+    padding: 0 15px;
+    transition: border-color .2s cubic-bezier(.645,.045,.355,1);
+    width: 60%;
+}
+
+.menu-item{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-left: 15px;
+  span{
+    padding: 0 5px;
+    font-size: 14px;
+  }
+  .cart-num{
+    width: 36px;
+    height: 18px;
+    border-radius: 18px;
+    text-align: center;
+    font-size: 14px;
+    background: #60c3d2;
+    color: #fff;
+    cursor: pointer;
+  }
+
+  .cart {
+      padding-right: 24px;
+      width: 54px;
+    }
+}
+.card-view{
+  input::-webkit-input-placeholder {
+      color: red;
+      font-size: 12px;
+      text-align: right;
+     }
+}
+
+
+</style>
+<style lang="less">
+.menu-type{
+  .el-input__icon {
+      height: 100%;
+      width: 25px;
+      text-align: center;
+      -webkit-transition: all .3s;
+      transition: all .3s;
+      line-height: 31px;
+  }
+  .el-select-dropdown__item.selected {
+    color: #60c3d2;
+    font-weight: 700;
+  }
+}
+.sort-view{
+  padding-left:16px;
+  .menu-item{
+    padding-left:30px;
+  }
+  .el-input__inner {
+    -webkit-appearance: none;
+    background-color: #fff;
+    background-image: none;
+    border-radius: 4px;
+    border: 2px solid #60c3d2;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+    color: #606266;
+    display: inline-block;
+    font-size: inherit;
+    height: 30px;
+    line-height: 31px;
+    outline: 0;
+    padding: 0 15px;
+    -webkit-transition: border-color .2s cubic-bezier(.645,.045,.355,1);
+    transition: border-color .2s cubic-bezier(.645,.045,.355,1);
+    width: 80%;
+  }
+
+}
+.card-view{
+  .el-input__inner {
+    -webkit-appearance: none;
+    background-color: #f6f6f6;
+    background-image: none;
+    border-radius: 4px;
+    border: none;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+    color: #606266;
+    display: inline-block;
+    font-size: inherit;
+    height: 30px;
+    line-height: 31px;
+    outline: 0;
+    padding: 0 15px;
+    -webkit-transition: border-color .2s cubic-bezier(.645,.045,.355,1);
+    transition: border-color .2s cubic-bezier(.645,.045,.355,1);
+    width: 100%;
+  }
+  input::-webkit-input-placeholder ,input{
+      font-weight:bold;
+      text-align: center;
+     }
+}
+
+</style>
+
